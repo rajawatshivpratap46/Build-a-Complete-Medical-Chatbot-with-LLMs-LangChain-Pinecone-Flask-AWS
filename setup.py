4b1ac954382e8d43b3646d73129f9cbd4756ac92
rajawatshivpratap46 <shiva18021912@gmail.com>
2026-03-20 21:41:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Shivkumar Singh Rajawat",
+    author_email="shiva18021912@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)   
